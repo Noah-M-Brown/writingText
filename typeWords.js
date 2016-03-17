1d61942c6ee36ad58186339e5b3893c32a4ea7b5
wowsk <wowsk14159@gmail.com>
2016-03-17 17:28:30 -0500

    Update typeWords.js

--- typeWords.js
+++ typeWords.js
@@ -3,9 +3,8 @@ var wowsk_typingLibrary_on=0;
 function typeWords(wordsToType,el) {
   el.textContent+=wordsToType[wowsk_typingLibrary_on];
   wowsk_typingLibrary_on++;
-  if(wowsk_typingLibrary_on===wordsToType.length) {
+  if(wowsk_typingLibrary_on===wordsToType.length) 
     window.clearInterval(wowsk_typingLibrary_type);
-  }
 }
 function startTyping(str,el,speed) {
   wowsk_typingLibrary_type=window.setInterval(
